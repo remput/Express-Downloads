fdd56e259eb69f9cb94dce6d763a9e3cff119bc9
remput <57086139+remput@users.noreply.github.com>
2020-02-11 11:58:02 +0800

    Update DownloadFilesMarketing.sh

--- Express_Downloads/Marketing/DownloadFilesMarketing.sh
+++ Express_Downloads/Marketing/DownloadFilesMarketing.sh
@@ -1,18 +1,17 @@
 sudo apt install -y git
-git clone https://github.com/remput/USB-security.git
+git clone https://github.com/remput/Jack-of-All-Trades.git
+mv Jack-of-All-Trades ~/Desktop
 sudo apt install -y curl
 curl -L https://xrl.us/installperlnix | bash
 sudo apt install cpanm
 sudo cpan Array::Utils
 cpan Array::Utils
 sudo apt install -y python-pip
-mv USB-security ~/Desktop
 sudo add-apt-repository ppa:unit193/encryption
 sudo add-apt-repository ppa:jtaylor/keepass
 sudo add-apt-repository ppa:otto-kesselgulasch/gimp
 sudo add-apt-repository ppa:libreoffice/ppa
 sudo apt-get -y update
-sudo apt install -y veracrypt
 sudo apt-get install -y keepass2
 sudo apt-get install -y gimp
 sudo apt install -y libreoffice
@@ -20,3 +19,4 @@ sudo apt install -y hashdeep
 sudo apt install -y zenity
 sudo apt install -y udisks
 cpan Array::Utils
+sudo apt-get install encfs
